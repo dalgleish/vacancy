670e0735b373bc3cddbc3f96f31cd8ba345bdc2d
Andrew Dalgleish <andrew@dalgleish.info>
2014-10-13 02:02:02 -0700

    Adding webapp
    - Based on twitter bootstrap 3.2.0
    - And socket.io 1.1.0

--- server/app.js
+++ server/app.js
@@ -1,18 +1,31 @@
-var express = require('express');
-var http = require('http');
 var util = require('util');
 var bodyParser = require('body-parser')
+
+var express = require('express.io');
+var app = express();
+app.http().io();
 
 var port = 8081;
 var status;
 
-var app = express();
+var rooms = {
+  men: {
+    status: 'offline'
+  },
+  women: {
+    status: 'offline'
+  }
+};
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
 app.post('/men', function(req, res) {
   if (req.body.status) {
-    status = req.body.status;
+    status = req.body.status.trim().toLowerCase();
+
+    // vacant or occupied
+    req.io.broadcast(status + ':men');
 
     console.log('Room is ', status);
   } else {
@@ -23,14 +36,19 @@ app.post('/men', function(req, res) {
   res.send('ok');
 });
 
-app.post('/women', function(req, res) {
-  res.send('hello world');
+// TODO: Not implemented
+//app.post('/women', function(req, res) { });
+
+app.io.route('reserve:men', function(req) {
+  console.log('reserve:men');
+  rooms.men.status = 'reserved';
+  app.io.broadcast('reserved:men');
+  // Everyone, except me:
+  //req.io.broadcast('reserved:men');
 });
 
-app.get('/', function(req, res) {
-
-  // TODO: Serve web content
-  res.send('hello world');
+app.io.route('ready', function(req) {
+  console.log('client connected', req.socket.id);
 });
 
 app.listen(port);
